7f839c738e74f120e9c077557537a7a6a90b3977
roman-sereda <Romansereda98@gmail.com>
2016-11-24 20:58:17 +0200

    Replace webpack to frontend folder

---
+++ frontend/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: __dirname + "/../uploads",
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: { presets: [ 'es2015', 'react' ] }
+      }
+    ]
+  }
+};
