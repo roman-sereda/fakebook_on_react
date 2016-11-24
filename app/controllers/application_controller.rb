ef2e6dd66575e5da5472a3d5bc544e813cfe9baf
roman-sereda <Romansereda98@gmail.com>
2016-11-24 22:13:33 +0200

    Move application.html from views to frontend and rename it to index.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def shit
-    render "layouts/application" 
+    render :file => 'frontend/index.html', :layout => false
   end
 end
