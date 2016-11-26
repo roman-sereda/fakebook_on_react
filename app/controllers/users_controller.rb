21642ceb06454e367116cc7cf6843dd06c1d203e
roman-sereda <Romansereda98@gmail.com>
2016-11-26 20:24:43 +0200

    Addition commit to previous

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,20 @@ class UsersController < ActionController::Base
     @user = User.find(params[:id])
     render json: @user.to_json
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      @user.sign_in
+      render json: @users.to_json
+    else
+      p @user.errors.full_messages
+      render json: @users.to_json
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :surname, :email, :password, :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   root "application#shit"
 
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
