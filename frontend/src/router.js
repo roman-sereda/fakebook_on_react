321dc331a8795fe9e1d0a7b93a567888c216230e
roman-sereda <Romansereda98@gmail.com>
2016-11-29 23:02:49 +0200

    change routes, without auth page

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
-import MainLayout from './components/layouts/main-layout';
-import LayoutAuth from './components/layouts/auth'
-import UsersContainer from './components/containers/user-list-container';
-import CurrentUserProfileContainer from './components/containers/current-user-profile-container';
-import UserProfileContainer from './components/containers/user-profile-container';
+import MainLayout                   from './components/layouts/main-layout';
+import LayoutAuth                   from './components/layouts/auth'
+import UsersContainer               from './components/containers/user-list-container';
+import CurrentUserProfileContainer  from './components/containers/current-user-profile-container';
+import UserProfileContainer         from './components/containers/user-profile-container';
 
-import SignIn from './components/containers/sign-in-container';
-import SignUp from './components/containers/sign-up-container';
+import SignIn                       from './components/containers/sign-in-container';
+import SignUp                       from './components/containers/sign-up-container';
 
 function requireAuth(){
   return (nextState, replace) => {
@@ -19,16 +19,13 @@ function requireAuth(){
 
 export default (
   <Router history={browserHistory}>
-      <Route component={MainLayout} >
-        <Route path="/" component={CurrentUserProfileContainer} onEnter={requireAuth()} />
-        <Route path="users">
-          <Route path=":userId" component={UserProfileContainer} />
+    <Route component={MainLayout} >
+      <Route path="/" component={CurrentUserProfileContainer} onEnter={requireAuth()} >
+        <Route path = ":userId" component={UserProfileContainer} >
+          <Route path = "photos" />
+          <Route path = "friends" />
         </Route>
-
-        <Route path="auth" component={LayoutAuth}>
-          <IndexRoute component={SignIn} />
-          <Route path="signup" component={SignUp} />
-        </Route>
+      </Route>
     </Route>
   </Router>
 );
