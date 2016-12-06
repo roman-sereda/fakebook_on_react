00ae1ca88f08a3d27e6c129fc15943523f66ddd8
roman-sereda <Romansereda98@gmail.com>
2016-12-06 19:05:41 +0200

    Edit friendship's controller. Check if there is this friendship. Doesn't send useless data

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -12,11 +12,18 @@ class FriendshipsController < ApplicationController
   def show
     user = User.find(params[:id])
     p "FRINEDSHIP: #{user.friendships}"
-    @friendship = user.friendships
+    @friendship = user.friendships.map {|friend|
+      user_temp = User.find(friend.friend_id)
+      {id: friend.friend_id,
+       login: "#{user_temp.name} #{user_temp.surname}",
+       avatar: user_temp.avatar.url}}
     render json: @friendship.to_json
   end
 
   def create
+    if (Friendship.where(:user_id => params[:user_id], :friend_id => params[:friend_id]).exists?)
+      render json: @friendship.to_json
+    end
     @friendship = User.find(params[:user_id]).friendships.create(:friend_id => params[:friend_id])
     @friendship2 = User.find(params[:friend_id]).friendships.create(:friend_id => params[:user_id])
     if @friendship.save && @friendship2.save
