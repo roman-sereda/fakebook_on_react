d4fe06cb6d45614b2c323d0a5e5376ca15bc7169
roman-sereda <Romansereda98@gmail.com>
2016-12-08 22:09:30 +0200

    Add separate navbar for logged user

---
+++ frontend/src/components/layouts/logged-navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default React.createClass({
+  render: function() {
+    return (
+      <div className="app">
+            <ul>
+              <li className="right"><Link to="/users" className="btn btn-default">All users</Link></li>
+              <li className="right"><Link to="/" activeClassName="active">Profile</Link></li>
+              <li className="right"><Link to="/gallery" activeClassName="active">All users</Link></li>
+              <li className="right"><a href='#' onClick={this.props.SignOut} >Log Out</a></li>
+            </ul>
+      </div>
+      );
+  }
+});
