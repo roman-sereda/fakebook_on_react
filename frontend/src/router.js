d70cbcf61a1d6d78b9353e4a31d3d6988a98a03e
roman-sereda <Romansereda98@gmail.com>
2016-12-09 09:33:07 +0200

    frontend/src/components/containers/

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,32 +1,31 @@
 import React from 'react';
+import store from './store';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
-import MainLayout from './components/containers/main-layout-container';
-import UsersContainer from './components/containers/user-list-container';
-import CurrentUserProfileContainer from './components/containers/current-user-profile-container';
-import UserProfileContainer from './components/containers/user-profile-container';
-import store from './store';
-import SignIn from './components/containers/sign-in-container';
-import SignUp from './components/containers/sign-up-container';
-import Temp from './components/containers/temp';
-import FriendsList from './components/containers/friends-list-container';
-import Gallery from './components/containers/gallery-list-container'
+import Navbars          from './components/containers/Navbars';
+import Users            from './components/containers/Users';
+import User             from './components/containers/Users';
+import SignIn           from './components/containers/SignIn';
+import SignUp           from './components/containers/SignUp';
+import MRouter           from './components/containers/Router';
+import FriendsList      from './components/containers/FriendsList';
+import Gallery          from './components/containers/Gallery'
 
 export default (
   <Router history={hashHistory}>
-    <Route component={MainLayout}>
-      <Route path="/" component={Temp} />
+    <Route component={Navbars}>
+      <Route path="/" component={MRouter} />
 
         <Route path="users">
-          <IndexRoute component={UsersContainer} />
+          <IndexRoute component={Users} />
           <Route path=":userId" >
-            <IndexRoute component={UserProfileContainer} />
+            <IndexRoute component={User} />
             <Route path="friends" component={FriendsList} />
             <Route path="photos" component={Gallery} />
           </Route>
         </Route>
 
-    
+
       <Route path="/auth/signin" component={SignIn} />
       <Route path="/auth/signup" component={SignUp} />
     </Route>
