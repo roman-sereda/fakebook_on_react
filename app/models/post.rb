56caa48c5c307981a0cfbb57b4a2a516f508e46d
roman-sereda <Romansereda98@gmail.com>
2016-12-09 22:14:12 +0200

    Add validation for all types of posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,26 @@ class Post < ActiveRecord::Base
 
   default_scope -> { order('created_at DESC') }
 
+  validates :title, presence: true, if: :has_photo?
+  validates :body, presence: true, if: :has_photo?
+  validates :images, presence: true, if: :has_text?
   validates :title, length: {minimum: 2, maximum: 25}
   validates :body, length: {maximum: 200}
+  validates :body, format: { without: /(((https?)?:\057\057)|(www.))[a-z_.]+[a-z_]{2,3}/i },if: :has_video?
+  validates :title, format: { without: /(((https?)?:\057\057)|(www.))[a-z_.]+[a-z_]{2,3}/i },if: :has_video?
+
+
+  def has_photo?
+    photo == nil
+  end
+
+  def has_video?
+    body =~ /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/
+  end
+
+  def has_text?
+    body == nil && title == nil
+  end
 
   def self.from_users_followed_by(user)
     friend_ids = user.friends{|a| a.id} | user.inverse_friends{|a| a.id}
