cd6027ed813ae115abffedddc5b63e76a28c8da5
roman-sereda <Romansereda98@gmail.com>
2016-12-09 15:09:25 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208171917) do
+ActiveRecord::Schema.define(version: 20161209122735) do
 
   create_table "friendships", force: :cascade do |t|
     t.integer  "user_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20161208171917) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "photo_id"
+    t.string   "video_url"
   end
 
   add_index "posts", ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
