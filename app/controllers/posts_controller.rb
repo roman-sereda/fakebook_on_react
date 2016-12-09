1f2c91675e4a7c1842214793ad407e45af0861d8
roman-sereda <Romansereda98@gmail.com>
2016-12-09 15:22:25 +0200

    Now it's send friend's posts too

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,14 +3,16 @@ class PostsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    @user = User.find(params[:user_id])
-    posts = @user.posts
-    @posts = posts.map {|post| {
+    user = User.find(params[:user_id])
+    posts = user.feed
+    @posts = posts.map {|post|
+      post_user = User.find(post.user_id)
+      {
       title: post.title,
       body: post.body,
       id: post.id,
-      user_login: "#{@user.name} #{@user.surname}",
-      user_avatar: @user.avatar,
+      user_login: "#{post_user.name} #{post_user.surname}",
+      user_avatar: post_user.avatar,
       photo: (post.photo_id != nil ? Photo.find(post.photo_id).image.url : nil),
       video_url: post.video_url}
     }
@@ -31,11 +33,11 @@ class PostsController < ApplicationController
                               user_id: params[:user_id], image: params[:image])
 
       @post = User.find(params[:user_id]).posts.create(title: params[:title],
-          body: params[:body], photo_id: @photo.id,
+          body: params[:body], photo_id: @photo.id, user_id: params[:user_id],
           video_url: if_has_video(params[:body]) ? get_url(params[:body]) : nil)
     else
       @post = User.find(params[:user_id]).posts.create(title: params[:title],
-          body: params[:body], photo_id: nil,
+          body: params[:body], photo_id: nil, user_id: params[:user_id],
           video_url: if_has_video(params[:body]) ? get_url(params[:body]) : nil)
 
       if(if_has_video(params[:body]))
